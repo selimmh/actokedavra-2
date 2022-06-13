a8c379ad73d3586343411b9a82eb4c0dea694ffb
selimmh <selim.mht@gmail.com>
2022-06-13 22:24:27 +0300

    Date added to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,15 @@
 import React from "react";
+import moment from "moment";
 
 // styles
 import styles from "./Footer.module.scss";
 
 function Footer() {
+  let time = moment().format("LL");
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.date}>Time</h3>
+      <h3 className={styles.date}>{time}</h3>
       <h2 className={styles.logo}>ActoKedavra</h2>
     </div>
   );
